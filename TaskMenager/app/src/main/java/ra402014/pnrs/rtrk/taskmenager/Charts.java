bd3a1c4501cea58b3d42550f371caa3d7a902021
miroslavradakovicRA402014 <miroslav.radakovic@gmail.com>
2017-04-22 20:59:54 +0200

    Crtanje

---
+++ TaskMenager/app/src/main/java/ra402014/pnrs/rtrk/taskmenager/Charts.java
@@ -0,0 +1,17 @@
+package ra402014.pnrs.rtrk.taskmenager;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by mika on 22.4.17..
+ */
+
+public class Charts extends View {
+
+
+    public Charts(Context context) {
+        super(context);
+    }
+
+}
--- TaskMenager/app/src/main/java/ra402014/pnrs/rtrk/taskmenager/Stat.java
+++ TaskMenager/app/src/main/java/ra402014/pnrs/rtrk/taskmenager/Stat.java
@@ -1,13 +1,28 @@
 package ra402014.pnrs.rtrk.taskmenager;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Stat extends Activity {
+
+    protected Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stat);
+        backButton = (Button) findViewById(R.id.backButton);
+
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(Stat.this,MainActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
